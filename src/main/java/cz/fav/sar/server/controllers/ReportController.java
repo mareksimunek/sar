c6814746142674198fe6bfba8b4669858114b835
Jindrich Pouba <pouba@students.zcu.cz>
2016-11-02 23:02:35 +0100

    Moved password to DBConfig.java

--- src/main/java/cz/fav/sar/server/controllers/ReportController.java
+++ src/main/java/cz/fav/sar/server/controllers/ReportController.java
@@ -4,21 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import cz.fav.sar.server.dao.PersonRepository;
+import cz.fav.sar.server.dao.ReportRepository;
 import cz.fav.sar.server.domain.Person;
+import cz.fav.sar.server.domain.Report;
 
-@Controller
+@RestController
 public class ReportController {
-	@Autowired
-	PersonRepository repository;
 	
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public @ResponseBody String get() {
-		Person p = repository.findOne(1L);	// selects person with id 1 from db
-		//repository.save(new Person());	// inserts new person into db
-		return "AAA";
+	@Autowired
+	ReportRepository reportRepository;
+	
+	@RequestMapping(value = "/report", method = RequestMethod.GET)
+	public Report get(@RequestParam("id") long id) {
+		Report r = reportRepository.findOne(id);	// selects person with id 1 from db
+		return r;
 	}
 
 }
--- src/main/java/cz/fav/sar/server/main/RepositoryConfig.java
+++ src/main/java/cz/fav/sar/server/main/RepositoryConfig.java
@@ -1,11 +1,25 @@
 package cz.fav.sar.server.main;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
-
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 
-@ImportResource({"classpath:Beans.xml"})
-public class RepositoryConfig {}
+@ImportResource({ "classpath:Beans.xml" })
+public class RepositoryConfig {
+
+	@Bean("dataSource")
+	public static DriverManagerDataSource getDataSource() {
+		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource(
+				DBConfig.URL, 
+				DBConfig.USERNAME, 
+				DBConfig.PASSWORD);
+
+		driverManagerDataSource.setDriverClassName("org.postgresql.Driver");
+		return driverManagerDataSource;
+
+	}
+
+}
